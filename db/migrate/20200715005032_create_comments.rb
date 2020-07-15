6186c8822db619dce51be61195f469fe43a4a42d
Melissa Williams <melissa@brandmeetscopy.com>
2020-07-14 17:58:24 -0700

    migrated database

--- db/migrate/20200715005032_create_comments.rb
+++ db/migrate/20200715005032_create_comments.rb
@@ -2,8 +2,9 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.string :content
-      t.resources :user
-      t.resources :project
+      t.references :user, foreign_key: true
+      t.references :project, foreign_key: true 
+
 
       t.timestamps
     end
