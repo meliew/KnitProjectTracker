b41699e1b61b30a320846155093e7ea83add99c0
Melissa Williams <melissa@brandmeetscopy.com>
2020-07-14 18:05:34 -0700

    added source for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :projects 
   has_many :categories, through: :projects 
   has_many :comments
-  has_many :commented_projects, through :comments 
+  has_many :commented_projects, through :comments, 
+  source: :project 
 
 end
